40b45739b8d4cd8cdfc664d3384a34390298252c
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-02 15:36:24 -0300

    feat: Criando nosso primeiro model

--- spring-example/src/main/java/br/com/treinaweb/spring_example/TesteController.java
+++ spring-example/src/main/java/br/com/treinaweb/spring_example/TesteController.java
@@ -1,13 +1,16 @@
 package br.com.treinaweb.spring_example;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TesteController {
@@ -40,5 +43,24 @@ public class TesteController {
         return "form";
     }
     
+    //@RequestParam não é obrigatório quando tem o mesmo nome
+    @GetMapping("/teste5")
+    public String action5(
+        @RequestParam(
+            name = "q", required = false, defaultValue = "Gustavo"
+        ) String q,
+        @RequestParam int age,
+        @RequestParam List<String> skills
+    ) {
+        System.out.println(q);
+        System.out.println(age);
+        System.out.println(skills);
+        return "teste";
+    }  
     
+    @GetMapping("/teste6/{id}")
+    public String action6(@PathVariable Long id) {
+        System.out.println(id);
+        return "teste";
+    }
 }
