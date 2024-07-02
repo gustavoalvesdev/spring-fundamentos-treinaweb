33ee4b93036fbef2fb0db29ab1d36621629a007d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-02 19:35:44 -0300

    feat: twprojects - Criando a página de listagem de clients

---
+++ twprojects/src/main/java/br/com/treinaweb/twprojects/web/clients/dtos/ClientViewModel.java
@@ -0,0 +1,32 @@
+package br.com.treinaweb.twprojects.web.clients.dtos;
+
+import br.com.treinaweb.twprojects.core.models.Client;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientViewModel {
+    
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public String getPhone() {
+        return phone.replaceAll("(\\d{2})(\\d{5})(\\d{4})", "($1) $2-$3");
+    }
+
+    public static ClientViewModel of(Client client) {
+        return  ClientViewModel.builder()
+                .id(client.getId())
+                .name(client.getName())
+                .phone(client.getPhone())
+                .email(client.getEmail())
+                .build();
+    }
+}
