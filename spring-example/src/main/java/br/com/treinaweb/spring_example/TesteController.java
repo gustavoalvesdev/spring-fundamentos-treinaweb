db6d3e86a52a0d197b7eeedb31e5bbd687025e37
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-01 21:27:47 -0300

    feat: Enviando dados da View para o Controller

--- spring-example/src/main/java/br/com/treinaweb/spring_example/TesteController.java
+++ spring-example/src/main/java/br/com/treinaweb/spring_example/TesteController.java
@@ -3,9 +3,11 @@ package br.com.treinaweb.spring_example;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TesteController {
@@ -26,4 +28,17 @@ public class TesteController {
         return new ModelAndView("teste", Map.of("nome", "Gustavo"));
     }
 
+    @GetMapping("/teste3")
+    public String action3(Model model) {
+        model.addAttribute("client", new Client("Gustavo", 32));
+        return "form";
+    }
+
+    @PostMapping("/teste3")
+    public String action4(Client client) {
+        System.out.println(client);
+        return "form";
+    }
+    
+    
 }
