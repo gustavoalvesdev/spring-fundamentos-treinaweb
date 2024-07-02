698e94d88450ba8a2588e5f27429662f4a87da79
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-02 16:25:51 -0300

    chore: Entendendo a injeção de dependências do Spring

--- spring-example/src/main/java/br/com/treinaweb/spring_example/SpringExampleApplication.java
+++ spring-example/src/main/java/br/com/treinaweb/spring_example/SpringExampleApplication.java
@@ -1,13 +1,27 @@
 package br.com.treinaweb.spring_example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExampleApplication {
+public class SpringExampleApplication implements CommandLineRunner {
+
+	
+	private final ClientRepository clientRepository;
+
+	public SpringExampleApplication(ClientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(clientRepository);
+		System.out.println("A aplicação iniciou");
 	}
 
 }
